38e04bd01ba7005d228fb505ff89cdc39422b5c6
vedsankhala17-design <vedsankhala17@gmail.com>
2026-03-09 10:17:32 +0530

    Add basic game start logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tiles = document.querySelectorAll(".tile");
 
+    // start with two tiles
+    addNumber();
+    addNumber();
+
+    function addNumber() {
+        let emptyTiles = [];
+
+        tiles.forEach(tile => {
+            if (tile.innerText === "") {
+                emptyTiles.push(tile);
+            }
+        });
+
+        if (emptyTiles.length > 0) {
+            let randomTile = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+            randomTile.innerText = "2";
+        }
+    }
+});
